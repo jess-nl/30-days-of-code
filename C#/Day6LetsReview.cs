436ac1ef692e3ec515c0ce1b5e0983df43b9afad
Jess Noriega-Lessard <jess@MacBook-Pro.local>
2021-06-01 11:49:53 -0400

    complete day 6 in c# and java

---
+++ C#/Day6LetsReview.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string s = Console.ReadLine();
+
+            for (int j = 0; j < s.Length; j++)
+            {
+                if (j % 2 == 0)
+                    Console.Write(s[j]);
+            }
+
+            Console.Write(" ");
+
+            for (int j = 0; j < s.Length; j++)
+            {
+                if (j % 2 != 0)
+                    Console.Write(s[j]);
+            }
+
+            Console.Write("\n");
+        }
+    }
+}
